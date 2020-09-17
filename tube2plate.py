0af9237c27b761b0641cb207744e754c1fc8838e
David Zong <davidzong1@gmail.com>
2020-09-17 10:23:03 -0500

    changed to do 96 well block to 96 well block

--- tube2plate.py
+++ tube2plate.py
@@ -12,31 +12,37 @@ metadata = {
 # where to look for autocomplete suggestions
 def run(protocol: protocol_api.ProtocolContext):
 
+    numtubes = 24 # leave at 24 until last round
+    round = 1 # increase to access further wells
+
+
+    adjustment = round * 24
     # labware
-    tubes1 = protocol.load_labware('opentrons_24_aluminumblock_nest_1.5ml_snapcap', '1')
-    tubes2 = protocol.load_labware('opentrons_24_aluminumblock_nest_1.5ml_snapcap', '2')
-    plate = protocol.load_labware('opentrons_96_aluminumblock_generic_pcr_strip_200ul', '3')
-    tiprack = protocol.load_labware('opentrons_96_tiprack_20ul', '5')
+    tubes = protocol.load_labware('opentrons_96_aluminumblock_generic_pcr_strip_200ul', '1')
+    plate = protocol.load_labware('opentrons_96_aluminumblock_generic_pcr_strip_200ul', '2')
+    tiprack = protocol.load_labware('opentrons_96_tiprack_20ul', '3')
 
     # pipettes
     pipette = protocol.load_instrument(
          'p20_single_gen2', 'right', tip_racks=[tiprack])
 
+     locations = ['A1','C1','E1','G1',
+                'A3','C3','E3','G3',
+                'A5','C5','E5','G5',
+                'A7','C7','E7','G7',
+                'A9','C9','E9','G9',
+                'A11','C11','E11','G11'
+    ]
+    pipette.flow_rate.aspirate = 3
+    pipette.well_bottom_clearance.aspirate = 1.5
     # commands
     # process first tube rack
-    for i in range(24):
+    for i in range(numtubes):
         pipette.pick_up_tip()
-        pipette.aspirate(20, tubes1.wells()[i].bottom())
-        pipette.dispense(20, plate.wells()[i].bottom())
-        pipette.aspirate(20, tubes1.wells()[i].bottom())
-        pipette.dispense(20, plate.wells()[i].bottom())
-        pipette.drop_tip()
-
-    # process second tube rack
-    for i in range(24):
-        pipette.pick_up_tip()
-        pipette.aspirate(20, tubes2.wells()[i].bottom())
-        pipette.dispense(20, plate.wells()[i+24].bottom())
-        pipette.aspirate(20, tubes2.wells()[i].bottom())
-        pipette.dispense(20, plate.wells()[i+24].bottom())
+        pipette.aspirate(20, tubes1[locations[i]])
+        pipette.dispense(20, plate.wells()[i+adjustment].bottom())
+        pipette.touch_tip(v_offset=-5)
+        pipette.aspirate(10, tubes1[locations[i]])
+        pipette.dispense(10, plate.wells()[i+adjustment].bottom())
+        pipette.touch_tip(v_offset=-5)
         pipette.drop_tip()
